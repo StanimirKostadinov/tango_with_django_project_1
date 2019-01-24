8c48a684207c46c4a70d264236671d7947b44a97
Stnaimir Kostadinov <2264064k@student.gla.ac.uk>
2019-01-24 09:08:03 +0000

    django_app url mapping

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-]
+	url(r'^django_app/', include('rango.urls')),
+	]
