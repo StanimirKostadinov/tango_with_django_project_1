79a01172693dcfda6cb071b2fad822bddfd4bf9d
Stnaimir Kostadinov <2264064k@student.gla.ac.uk>
2019-01-25 08:06:37 +0000

    django_app urls spelling correction

--- django_app/urls.py
+++ django_app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from django_app  import views
 
 urlpatterns = [
-    url(r'^S',  views.index, name = 'index_django),
+    url(r'^S',  views.index_django, name = 'index_django),
     ]
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
-from djnago_app import views
+from django_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
